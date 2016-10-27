b5a894a7c53ba44ce94f1788ff9ab4d2a9c690d8
Bebabo10 <Nylegarcia01@gmail.com>
2016-10-27 00:19:48 -0500

    fixing bugs

--- pregen.sh
+++ pregen.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-session=${1:-"0"}
+session=${1:-"0"}:0
 delay=${2:-"10"}
 border=${3:-"1000"}
 user=${4:-""}
 
-tmux send-keys -t $session 'gamemode $user 1' Enter
+tmux send-keys -t $session 'gamemode '$user' 1' Enter
 
 for (( r=$border; r>-$border ; r=r-16 )) do
-	for (( =$border; c>-$border ; c=r-16 )) do
+        for (( c=$border; c>-$border ; c=r-16 )) do
 
-		tmux send-keys -t $session 'tppos $user $r 120 $c' Enter
-		
-		sleep $delay
+                tmux send-keys -t $session 'tppos '$user' '$r' 120 '$c' -f' Enter
 
-
+                sleep $delay
 
 
-	done
+
+
+        done
 
 
 
